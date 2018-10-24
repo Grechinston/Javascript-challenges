993ac7466c067e7d745c93089aba176e32845808
Florentin <39676852+Grechinston@users.noreply.github.com>
2018-10-24 12:27:02 +0100

    Update script.js

--- Sorting-an-array/script.js
+++ Sorting-an-array/script.js
@@ -1,19 +1,19 @@
 
 
-var x = [5, 3, 4, 1, 2 ];
+var x = [ 5, 3, 4, 1, 2 ];
 
-for ( i = 0; i <= x.length; i ++ ){
-    for( j = 0; j < i; j++){
+for ( i = 0;  i <= x.length;  i ++ ){
+    for( j = 0; j < i; j++ ){
 
-    if (x [ i ] < x [ j ] ){
-        var y = x[i];
-        x [i] = x [j];
-        x[j] = y
+    if ( x [ i ] < x [ j ] ){
+        var y = x [i] ;
+        x [i] = x [j] ;
+        x [j] = y
     } 
         
     }
         
     }
-console.log(x);
+console.log( x );
 
  
