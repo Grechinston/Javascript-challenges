2c12ac409a0c955a63cb8ff475fdf428f7fb7ea1
Florentin <39676852+Grechinston@users.noreply.github.com>
2018-10-24 12:25:50 +0100

    Create script.js

---
+++ Sorting-an-array/script.js
@@ -0,0 +1,19 @@
+
+
+var x = [5, 3, 4, 1, 2 ];
+
+for ( i = 0; i <= x.length; i ++ ){
+    for( j = 0; j < i; j++){
+
+    if (x [ i ] < x [ j ] ){
+        var y = x[i];
+        x [i] = x [j];
+        x[j] = y
+    } 
+        
+    }
+        
+    }
+console.log(x);
+
+ 
